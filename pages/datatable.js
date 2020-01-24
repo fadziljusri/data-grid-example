39a5c5517d20f4ae566adcf2f188400ce570911e
Fadzil Jusri <fadziljusri@gmail.com>
2020-01-24 20:56:56 +0800

    init

---
+++ pages/datatable.js
@@ -0,0 +1,129 @@
+const columns = [
+  {
+    label: "Row 1",
+    field: "row_1",
+    filterOptions: {
+      enabled: true,
+      placeholder: "-- All --",
+      filterValue: "" // initial value
+    }
+  },
+  {
+    label: "Row 2",
+    field: "row_2",
+    filterOptions: {
+      enabled: true,
+      placeholder: "-- All --",
+      filterValue: "" // initial value
+    }
+  },
+  {
+    label: "Row 3",
+    field: "row_3",
+    filterOptions: {
+      enabled: true,
+      placeholder: "-- All --",
+      filterValue: "" // initial value
+    }
+  },
+  {
+    label: "Row 4",
+    field: "row_4",
+    filterOptions: {
+      enabled: true,
+      placeholder: "-- All --",
+      filterValue: "" // initial value
+    }
+  },
+  {
+    label: "Row 5",
+    field: "row_5",
+    filterOptions: {
+      enabled: true,
+      placeholder: "-- All --",
+      filterValue: "" // initial value
+    }
+  },
+  {
+    label: "Row 6",
+    field: "row_6",
+    filterOptions: {
+      enabled: true,
+      placeholder: "-- All --",
+      filterValue: "" // initial value
+    }
+  },
+  {
+    label: "Row 7",
+    field: "row_7",
+    filterOptions: {
+      enabled: true,
+      placeholder: "-- All --",
+      filterValue: "" // initial value
+    }
+  },
+  {
+    label: "Row 8",
+    field: "row_8",
+    filterOptions: {
+      enabled: true,
+      placeholder: "-- All --",
+      filterValue: "" // initial value
+    }
+  },
+  {
+    label: "Row 9",
+    field: "row_9",
+    filterOptions: {
+      enabled: true,
+      placeholder: "-- All --",
+      filterValue: "" // initial value
+    }
+  },
+  {
+    label: "Row 10",
+    field: "row_10",
+    filterOptions: {
+      enabled: true,
+      placeholder: "-- All --",
+      filterValue: "", // initial value
+      // filterDropdownItems: [
+      //   { text: "Internal", value: "internal" },
+      //   { text: "Guest", value: "guest" }
+      // ]
+    }
+  },
+];
+
+const sortOptions = {
+  enabled: true,
+  initialSortBy: [{ field: "row_1", type: "asc" }]
+};
+
+const searchOptions = {
+  enabled: false,
+  placeholder: "Search this table"
+};
+
+const pageOptions = {
+  enabled: true,
+  mode: "records",
+  perPage: 20,
+  // position: "top",
+  // perPageDropdown: [3, 7, 9],
+  dropdownAllowAll: false,
+  setCurrentPage: 1,
+  nextLabel: "next",
+  prevLabel: "prev",
+  rowsPerPageLabel: "Rows per page",
+  ofLabel: "of",
+  pageLabel: "page", // for 'pages' mode
+  allLabel: "All"
+};
+
+export default {
+  columns,
+  sortOptions,
+  searchOptions,
+  pageOptions
+};
---
+++ pages/index.vue
@@ -0,0 +1,119 @@
+<template>
+  <div class="section">
+    <div class="container">
+      <!-- <logo /> -->
+
+      <vue-good-table
+        :columns="vgt.columns"
+        :rows="data"
+        :pagination-options="vgt.pageOptions"
+        :sort-options="vgt.sortOptions"
+        :search-options="vgt.searchOptions"
+        styleClass="vgt-table condensed bordered"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import Logo from "~/components/Logo.vue";
+import vgt from "./datatable";
+
+// import the styles
+import "vue-good-table/dist/vue-good-table.css";
+import { VueGoodTable } from "vue-good-table";
+
+export default {
+  components: {
+    Logo,
+    VueGoodTable
+  },
+  data() {
+    return {
+      vgt,
+      fixed: 5,
+      data: []
+    };
+  },
+  async created() {
+     for(let i = 0; i< 2000; i++) {
+        this.data.push({
+          row_1: (Math.random() * parseFloat(Math.random().toString().substr(-5))).toFixed(this.fixed),
+          row_2: (Math.random() * parseFloat(Math.random().toString().substr(-5))).toFixed(this.fixed),
+          row_3: (Math.random() * parseFloat(Math.random().toString().substr(-5))).toFixed(this.fixed),
+          row_4: (Math.random() * parseFloat(Math.random().toString().substr(-5))).toFixed(this.fixed),
+          row_5: (Math.random() * parseFloat(Math.random().toString().substr(-5))).toFixed(this.fixed),
+          row_6: (Math.random() * parseFloat(Math.random().toString().substr(-5))).toFixed(this.fixed),
+          row_7: (Math.random() * parseFloat(Math.random().toString().substr(-5))).toFixed(this.fixed),
+          row_8: (Math.random() * parseFloat(Math.random().toString().substr(-5))).toFixed(this.fixed),
+          row_9: (Math.random() * parseFloat(Math.random().toString().substr(-5))).toFixed(this.fixed),
+          row_10: (Math.random() * parseFloat(Math.random().toString().substr(-5))).toFixed(this.fixed)
+        })
+
+
+        if (i && i % 1000 === 0) {
+          await new Promise(resolve => setTimeout(function() {
+            resolve(true)
+          }, 1000));
+        }
+      }
+  },
+  // computed: {
+  //   // rows() {
+  //   //   return this.data;
+  //   // }
+  // }
+};
+</script>
+
+<style lang="scss">
+// Vue Good Table
+
+.vgt-wrap__footer,
+.vgt-global-search__input .vgt-input {
+  padding: 0.5em 1em !important;
+  line-height: 1;
+}
+
+.vgt-table {
+  .vgt-active-row {
+    background: aliceblue;
+  }
+
+  .th-actions {
+    width: 100px !important;
+    min-width: 100px !important;
+  }
+
+  .th-datetime {
+    min-width: 170px !important;
+  }
+
+  .th-100 {
+    min-width: 100px !important;
+  }
+
+  .th-150 {
+    min-width: 150px !important;
+  }
+
+  .th-180 {
+    min-width: 180px !important;
+  }
+
+  .th-280 {
+    min-width: 280px !important;
+  }
+}
+
+.buttons {
+  .button.action {
+    background: unset !important;
+    box-shadow: none !important;
+    border: none !important;
+    padding: 0 0.25rem;
+    margin-bottom: 0 !important;
+    height: unset; // margin-right: 0 !important
+  }
+}
+</style>
